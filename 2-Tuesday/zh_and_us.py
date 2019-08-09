d4e82538826d214ac621cd59d51046bc92835579
Jesse A. Tov <jesse.tov@gmail.com>
2019-08-09 10:06:48 +0800

    some additional comments

--- 2-Tuesday/zh_and_us.py
+++ 2-Tuesday/zh_and_us.py
@@ -23,6 +23,11 @@ def china(width: float):
     jump_to(old_x + width, old_y)
     seth(old_h)
     pendown()
+    
+    
+def is_even(z: int) -> bool:
+    """Determines whether an integer is even."""
+    return z % 2 == 0
 
 
 def usa(width: float,
@@ -31,26 +36,30 @@ def usa(width: float,
         rows: int = 9):
     """Draws the flag of the United States."""
     old_x, old_y = pos()
+    # Calculate various dimensions:
     height    = 2 * width / 3
-    stripe_ht = height / stripes
-    canton_ht = (stripes + 1) // 2 * stripe_ht
-    canton_wd = width / 2
-    star_dx   = canton_wd / columns
-    star_dy   = 0.9 * canton_ht / rows
-    star_y0   = old_y - 0.7 * star_dy
-    star_size = star_dx / 5
-    margin_x  = star_dx / 5
+    stripe_ht = height / stripes                # stripe thickness
+    canton_ht = (stripes + 1) // 2 * stripe_ht  # height of blue canton
+    canton_wd = width / 2                       # width of blue canton
+    star_dx   = canton_wd / columns             # horiz. displacement of stars
+    star_dy   = 0.9 * canton_ht / rows          # vert. displacement of stars
+    star_y0   = old_y - 0.7 * star_dy           # y coordinate of top star row
+    star_size = star_dx / 5                     # size of each star
+    margin_x  = star_dx / 5                     # star field left margin size
+    # Draw the stripes:
     penup()
     for row in range(stripes):
-        if row % 2 == 0:
+        if is_even(row):
             filled_rectangle(width, stripe_ht, 'red')
         else:
             filled_rectangle(width, stripe_ht, 'white')
         jump_by(0, -stripe_ht)
+    # Draw the blue canton:
     jump_to(old_x, old_y)
     filled_rectangle(canton_wd, canton_ht, 'blue')
+    # Draw the stars:
     for row in range(rows):
-        if row % 2 == 0:
+        if is_even(row):
             row_size = columns
             star_x0 = old_x + margin_x
         else:
@@ -61,6 +70,7 @@ def usa(width: float,
             y = star_y0 - row * star_dy
             jump_to(x, y)
             star(star_size, fill = 'white')
+    # Outline the whole thing:
     pendown()
     jump_to(old_x, old_y)
     rectangle(width, height)
